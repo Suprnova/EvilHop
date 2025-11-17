387060acdc44d6a6221349a3b75837907ad3ff7d
Suprnova <nova@suprnova.dev>
2025-11-16 22:49:30 -0500

    evilstring toevilbytes implementation

--- src/EvilHop/Extensions/EvilString.cs
+++ src/EvilHop/Extensions/EvilString.cs
@@ -1,4 +1,4 @@
-﻿using System.Buffers.Binary;
+﻿using System.Text;
 
 namespace EvilHop.Extensions;
 
@@ -10,10 +10,15 @@ public static class EvilString
         /// Returns the EvilEngine byte representation of this <see cref="string"/>.
         /// </summary>
         /// <returns>the EvilEngine byte representation of this <see cref="string"/>.</returns>
-        public byte[] ToEvilBytes() => throw new NotImplementedException();
+        public byte[] ToEvilBytes()
+        {
+            str = str.Length % 2 == 0 ? str + "\0\0" : str + '\0';
+            // todo: ascii or extended ascii?
+            return Encoding.ASCII.GetBytes(str);
+        }
     }
 
-    // TODO: bad in theory, since strings are null terminated?
+    // TODO: bad in theory, since strings are null terminated
     // we can't make a string from a slice if we don't know the dimensions of the slice
     extension(Span<byte> bytes)
     {
---
+++ tests/EvilHop.Tests/Extensions/EvilStringTests.cs
@@ -0,0 +1,12 @@
+﻿using EvilHop.Extensions;
+
+namespace EvilHop.Tests.Extensions;
+
+public class EvilStringTests
+{
+    [Theory]
+    [InlineData("", new byte[] { 0x00, 0x00 })]
+    [InlineData("abc", new byte[] { 0x61, 0x62, 0x63, 0x00 })]
+    [InlineData("abcd", new byte[] { 0x61, 0x62, 0x63, 0x64, 0x00, 0x00 })]
+    public void ToEvilBytes_ValidString_CorrectBytes(string str, byte[] expected) => Assert.Equal(expected, str.ToEvilBytes());
+}
