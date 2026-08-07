e717f3468bdd35d8e11dc6baaa76e5a9e5ca1ddc
Suprnova <nova@suprnova.dev>
2026-08-07 17:16:39 -0400

    minor spelling mistake

--- src/EvilHop/Archive.cs
+++ src/EvilHop/Archive.cs
@@ -3,7 +3,7 @@
 /// <summary>
 /// Represents a single HIP archive file in memory. This is the primary entry point for
 /// all consumers, providing both high-level asset manipulation capabilities alongside low-level
-/// block access through a dual-layer architrecture with modal access control.
+/// block access through a dual-layer architecture with modal access control.
 /// </summary>
 /// <remarks>
 /// <seealso href="https://heavyironmodding.org/wiki/EvilEngine/HIP_(File_Format)">Heavy Iron Modding documentation</seealso>
