351db61dbfbd15ece6e220ea16d389bcfd8c5450
Suprnova <nova@suprnova.dev>
2026-08-07 15:41:12 -0400

    basic block implementation

--- src/EvilHop/Archive.cs
+++ src/EvilHop/Archive.cs
@@ -1,10 +1,13 @@
 ﻿namespace EvilHop;
 
 /// <summary>
-/// Represents a single HIP/HOP archive file in memory. This is the primary entry point for
+/// Represents a single HIP archive file in memory. This is the primary entry point for
 /// all consumers, providing both high-level asset manipulation capabilities alongside low-level
 /// block access through a dual-layer architrecture with modal access control.
 /// </summary>
+/// <remarks>
+/// <seealso href="https://heavyironmodding.org/wiki/EvilEngine/HIP_(File_Format)">Heavy Iron Modding documentation</seealso>
+/// </remarks>
 public class Archive
 {
 
