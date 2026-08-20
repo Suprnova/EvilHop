79a20b727467f97813d0965957580e110510ee6b
Suprnova <nova@suprnova.dev>
2026-08-20 17:21:29 -0400

    correct PCRT timestamp assumptions

--- src/EvilHop/Blocks/Package.Serialization.cs
+++ src/EvilHop/Blocks/Package.Serialization.cs
@@ -36,23 +36,21 @@ public partial class SerializerV1
     }
 
     /// <summary>
-    /// Reads the fields of a <see cref="PackageCreated"/> (PCRT) block. <see cref="PackageCreated.CreatedDate"/>
-    /// is converted from a raw UTC Unix timestamp to a UTC-7:00 (Pacific Time) display offset.
+    /// Reads the fields of a <see cref="PackageCreated"/> (PCRT) block.
     /// </summary>
     protected virtual void ReadPackageCreated(BinaryReader reader, PackageCreated block, uint size)
     {
         uint rawCreatedDate = reader.ReadEvilInt();
-        block.CreatedDate = DateTimeOffset.FromUnixTimeSeconds(rawCreatedDate).ToOffset(TimeSpan.FromHours(-7));
+        block.CreatedDate = DateTimeOffset.FromUnixTimeSeconds(rawCreatedDate);
         block.CreatedDateString = reader.ReadEvilString();
     }
 
     /// <summary>
-    /// Reads the fields of a <see cref="PackageModified"/> (PMOD) block. <see cref="PackageModified.ModifiedDate"/>
-    /// is converted from a raw UTC Unix timestamp to a UTC-7:00 (Pacific Time) display offset.
+    /// Reads the fields of a <see cref="PackageModified"/> (PMOD) block.
     /// </summary>
     protected virtual void ReadPackageModified(BinaryReader reader, PackageModified block, uint size)
     {
         uint rawModifiedDate = reader.ReadEvilInt();
-        block.ModifiedDate = DateTimeOffset.FromUnixTimeSeconds(rawModifiedDate).ToOffset(TimeSpan.FromHours(-7));
+        block.ModifiedDate = DateTimeOffset.FromUnixTimeSeconds(rawModifiedDate);
     }
 }
